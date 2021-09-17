3cbabc02589824fb7a96dbd1504add93056d4da6
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-17 16:07:11 +0800

    Implement card component utilizing bootstrap framework on Home component

--- resources/js/src/components/Home.js
+++ resources/js/src/components/Home.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
     return (
-        <div className="Home__container">
-            This is Home component.
+        <div className="container">
+            <div class="card">
+                <div class="card-header">Featured</div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">
+                        With supporting text below as a natural lead-in to
+                        additional content.
+                    </p>
+                    <a href="#" class="btn btn-primary">
+                        Go somewhere
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
 
 export default Home;
-
