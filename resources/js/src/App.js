8e65f93956d9b077df4e90880f1cb613ce954a63
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-17 16:03:06 +0800

    utilized ReactDom on and BrowserRouter for declerating routing to Home Component

--- resources/js/src/App.js
+++ resources/js/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import  ReactDOM  from 'react-dom';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
-}
-from 'react-router-dom';
+} from 'react-router-dom';
+import Home from './components/Home';
 
 const App = () => {
     return (
-        <div className="App__container">
-            This is App component.
-        </div>
+        <Router className="App__container">
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                    </Route>
+            </Switch>
+        </Router>
     );
 };
+ReactDOM.render(<App />, document.getElementById('app'));
 
-export default App;
+
 
