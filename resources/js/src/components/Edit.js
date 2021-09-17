4aea234749d6a4fecb30b282cd5d437833d4c7cb
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-17 15:57:18 +0800

    Added Add, Edit, Delite Components, Deleted Example component

---
+++ resources/js/src/components/Edit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Edit = () => {
+    return (
+        <div className="Edit__container">
+            This is Edit component.
+        </div>
+    );
+};
+
+export default Edit;
+
