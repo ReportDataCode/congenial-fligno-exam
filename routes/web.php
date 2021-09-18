a715138241abc0a49d0f20665c0a70a4102f5392
Prince Anire <andrewcustomerhelper@gmail.com>
2021-09-18 19:49:56 +0800

    Added, Simple Database Seeder, add a simple slug routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+// 25:00 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,9 +14,21 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get(
+    '/home',
+    function () {
+        return view('home');
+    }
+);
+
+Route::get('/tutorials/{slug}', function ($slug) {
+    return $slug;
 });
+
+Route::get('/', function () {
+    
+    return view('welcome');})->where('any', '^(?!api).*$');
+
 
 Auth::routes();
 
